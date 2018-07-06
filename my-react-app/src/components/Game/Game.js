0860a3ea1169722e8a9e66918c2bc9bfb688f7b3
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-07-06 12:00:08 -0400

    update

--- my-react-app/src/components/Game/Game.js
+++ my-react-app/src/components/Game/Game.js
@@ -4,14 +4,9 @@ import Header from "../Header";
 import Container from "../Container";
 import ClickItem from "../ClickItem";
 import Footer from "../Footer";
-import data from "../../data.json";
 
 class Game extends Component {
-  state = {
-    data,
-    score: 0,
-    topScore: 0
-  };
+  
 
   componentDidMount() {
     this.setState({ data: this.shuffleData(this.state.data) });
--- my-react-app/src/components/Game/index.js
+++ my-react-app/src/components/Game/index.js
@@ -1,1 +1,1 @@
-export { default } from "../Game";
+export { default } from "/Users/ezdeharjaber/Documents/cwru/React-App/my-react-app/src/components/Game/Game.js";
